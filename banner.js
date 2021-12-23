afe102d9cddad5d23d422cf07939ef51c63ded1b
John Swan <johneswan3@protonmail.com>
2021-12-23 16:07:59 -0700

    Set up banner.js

---
+++ banner.js
@@ -0,0 +1,4 @@
+// Variables to manipulate banner
+const banner = document.getElementById('banner');
+const bannerTerm = document.getElementById('banner-term');
+const bannerDef = document.getElementById('banner-definition');
